f5a9d1dc9b1ce636d0edc6eeb619a9d64ad4f4a3
yash <yashboura303@gmail.com>
2020-02-09 01:15:04 +0530

    removed faulty pip file

--- superadmin/settings.py
+++ superadmin/settings.py
@@ -75,11 +75,15 @@ WSGI_APPLICATION = 'superadmin.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'superadmin',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
+    }}
 
 
 # Password validation
